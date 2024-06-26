a875aa033762c9914726c282e388a5b50581ae3d
Akshat Jain <akjain9758@gmail.com>
2024-06-26 10:20:57 +0530

    Projects Animation

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,9 @@
+import { useEffect, useRef } from "react";
 import project1 from "../../assets/project1.png";
 import project2 from "../../assets/project2.png";
 import project3 from "../../assets/project3.png";
 import "./Projects.css";
-import { motion } from "framer-motion";
+import { motion, useAnimation, useInView } from "framer-motion";
 
 const projects = [
   {
@@ -62,42 +63,67 @@ const Projects = () => {
       </motion.h1>
       <hr className="w-1/3 m-auto my-6 h-[2px] bg-black dark:bg-secondaryDarkText" />
       <ul className="w-[95%] md:w-[90%] lg:w-[80%] m-auto">
-        {projects.map((project) => (
-          <div
-            key={project.id}
-            className={`bg-white dark:bg-card dark:text-primaryDarkText lg:flex gap-6 justify-evenly items-center h-full rounded-lg dark:shadow-gray-700 dark:shadow-md shadow-lg p-4 my-6 ${
-              project.id % 2 === 0 ? "flex-row-reverse" : ""
-            }`}
-          >
-            <img
-              src={project.display}
-              alt="project image"
-              className=" hover:cursor-pointer hover:scale-105 duration-100 h-52 md:h-72 m-auto rounded-md drop-shadow-lg"
-              onClick={() => window.open(project.link, "_blank")}
-            />
-            <div className="p-4">
-              <h2 className="font-bold text-lg lg:text-xl">{project.title}</h2>
-              <p className=" text-sm italic dark:text-accent1">
-                {project.duration}
-              </p>
-              <ul className="list-disc list-inside text-justify dark:text-secondaryDarkText">
-                {project.desc.map((item) => (
-                  <li key={item.split(1)}>{item}</li>
-                ))}
-              </ul>
-              <a
-                href={project.link}
-                className="italic text-blue-600 dark:text-blue-400 hover:scale-105 duration-100 m-auto w-fit mt-3 block"
-                target="blank"
-                rel="noopener noreferrer"
-              >
-                Go to Website
-              </a>
-            </div>
-          </div>
-        ))}
+        {projects.map((project) => {
+          const projRef = useRef();
+          const isInView = useInView(projRef, { once: true });
+          const controls = useAnimation();
+
+          useEffect(() => {
+            if (isInView) controls.start("visible");
+          }, [isInView, controls]);
+
+          return (
+            <motion.div
+              ref={projRef}
+              key={project.id}
+              variants={{
+                hidden: { opacity: 0, translateY: 90 },
+                visible: { opacity: 1, translateY: 0 },
+              }}
+              initial="hidden"
+              animate={controls}
+              transition={{
+                type: "spring",
+                duration: 0.5,
+                stiffness: 50,
+              }}
+              className={`bg-white dark:bg-card dark:text-primaryDarkText lg:flex gap-6 justify-evenly items-center h-full rounded-lg dark:shadow-gray-700 dark:shadow-md shadow-lg p-4 my-6 ${
+                project.id % 2 === 0 ? "flex-row-reverse" : ""
+              }`}
+            >
+              <img
+                src={project.display}
+                alt="project image"
+                className="hover:cursor-pointer hover:scale-105 duration-100 h-52 md:h-72 m-auto rounded-md drop-shadow-lg"
+                onClick={() => window.open(project.link, "_blank")}
+              />
+              <div className="p-4">
+                <h2 className="font-bold text-lg lg:text-xl">
+                  {project.title}
+                </h2>
+                <p className="text-sm italic dark:text-accent1">
+                  {project.duration}
+                </p>
+                <ul className="list-disc list-inside text-justify dark:text-secondaryDarkText">
+                  {project.desc.map((item, index) => (
+                    <li key={index}>{item}</li>
+                  ))}
+                </ul>
+                <a
+                  href={project.link}
+                  className="italic text-blue-600 dark:text-blue-400 hover:scale-105 duration-100 m-auto w-fit mt-3 block"
+                  target="blank"
+                  rel="noopener noreferrer"
+                >
+                  Go to Website
+                </a>
+              </div>
+            </motion.div>
+          );
+        })}
       </ul>
     </div>
   );
 };
+
 export default Projects;
