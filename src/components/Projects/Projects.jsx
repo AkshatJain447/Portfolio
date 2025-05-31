412e5420a65817da8ed79ffac34ccd908cc265b3
Akshat Jain <133579304+AkshatJain447@users.noreply.github.com>
2025-05-31 19:53:34 +0530

    Update Projects.jsx for Project 3 URL

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -43,7 +43,7 @@ const projects = [
       "Developed several minor projects to reinforce skills in web application development",
       "Completed a major project focusing on Hotel Management System, facilitating room booking and management for prominent local hotels",
     ],
-    link: "https://eventeaseportal.netlify.app",
+    link: "https://akshatjain447.github.io/Event-Booking-Portal/",
   },
 ];
 
